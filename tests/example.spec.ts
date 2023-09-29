eafe60e6131afa2f2954bb38ee489081bba5e104
Tirivashe <shaymusts@gmail.com>
2023-09-29 18:38:55 +0200

    first test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,20 @@
 import { test, expect } from "@playwright/test";
 
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://shamhu-electronics-shop.vercel.app/"
+    : "http://localhost:3000";
+
 test.describe("home", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(url);
   });
 
   test("has title", async ({ page }) => {
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Next/);
+    await expect(page).toHaveTitle("My Mantine app");
+
+    const heading = page.getByRole("heading", { name: "Home" });
+    await expect(heading).toBeVisible();
   });
 });
