c6a2b0fdb083a130bac3afc22c6cf1203672b2d5
Tirivashe <shaymusts@gmail.com>
2023-10-02 12:26:04 +0200

    added new page description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 
 export const metadata = {
   title: "My Mantine app",
-  description: "I have followed setup instructions carefully",
+  description: "E-commerce store",
 };
 
 export default function RootLayout({
